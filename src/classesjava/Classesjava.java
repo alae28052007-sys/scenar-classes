cd564275fb6fd19a561c18e095b73ebe3e55aeff
Alae <alae28052007@gmail.com>
2026-04-07 15:30:43 +0200

    exo5

--- src/classesjava/Classesjava.java
+++ src/classesjava/Classesjava.java
@@ -14,21 +14,9 @@ public class Classesjava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Voiture v1 = new Voiture();
-        Voiture v2 = new Voiture();
-        Voiture v3 = new Voiture();
-        v1.marque = "Peugeot";
-        v1.modele = "208";
-        v1.annee = "2021";
-        v1.prix = 14500 ;
-        v2.marque = "Toyota";
-        v2.modele = "yaris";
-        v2.annee = "2015";
-        v2.prix = 9800 ;
-        v3.marque = "Renault";
-        v3.modele = "clio";
-        v3.annee = "2000";
-        v3.prix = 3500 ;
+        Voiture v1 = new Voiture("Peugeot", "208", 2021, 14500);
+        Voiture v2 = new Voiture("Toyota", "yaris", 2015, 9800);
+        Voiture v3 = new Voiture("renault", "clio", 2000, 3500);
         
         }
     
