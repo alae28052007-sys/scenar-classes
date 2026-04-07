5069d709a2c28ee46105e5652f0398c66215067f
Alae <alae28052007@gmail.com>
2026-04-07 15:17:29 +0200

    exo4

--- src/classesjava/Classesjava.java
+++ src/classesjava/Classesjava.java
@@ -14,11 +14,22 @@ public class Classesjava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        public class Voiture {
-            String marque;
-            String modele;
-            int annee;
-            double prix;
+        Voiture v1 = new Voiture();
+        Voiture v2 = new Voiture();
+        Voiture v3 = new Voiture();
+        v1.marque = "Peugeot";
+        v1.modele = "208";
+        v1.annee = "2021";
+        v1.prix = 14500 ;
+        v2.marque = "Toyota";
+        v2.modele = "yaris";
+        v2.annee = "2015";
+        v2.prix = 9800 ;
+        v3.marque = "Renault";
+        v3.modele = "clio";
+        v3.annee = "2000";
+        v3.prix = 3500 ;
+        
         }
     
 }
